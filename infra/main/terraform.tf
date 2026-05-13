f05147c7a88e1fec2b653ef429e5764a784b6cc6
Andy Holt <andrew.holt@hotmail.co.uk>
2026-05-14 00:49:36 +0100

    initialise main terraform with remote state

---
+++ infra/main/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+
+  # Remote state in the bucket created by infra/bootstrap.
+  # Backend blocks cannot reference variables, so the bucket name is hardcoded.
+  backend "gcs" {
+    bucket = "will-it-rain-496215-tfstate"
+    prefix = "main"
+  }
+}
---
+++ infra/main/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  type        = string
+  description = "GCP project ID."
+  default     = "will-it-rain-496215"
+}
+
+variable "region" {
+  type        = string
+  description = "Default region for regional resources."
+  default     = "europe-west2"
+}
