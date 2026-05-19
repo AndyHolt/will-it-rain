c21fb546120b4611d0b1f731793072b26d43f35f
Andy Holt <andrew.holt@hotmail.co.uk>
2026-05-19 10:42:39 +0100

    configure infra for hosting frontend in firebase

--- infra/main/main.tf
+++ infra/main/main.tf
@@ -4,3 +4,10 @@ provider "google" {
   billing_project       = var.project_id
   user_project_override = true
 }
+
+provider "google-beta" {
+  project               = var.project_id
+  region                = var.region
+  billing_project       = var.project_id
+  user_project_override = true
+}
