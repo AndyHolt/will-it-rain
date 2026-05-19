eff6a729f575306a9ad4a4c65570f89c20dfd149
Andy Holt <andrew.holt@hotmail.co.uk>
2026-05-19 09:56:21 +0100

    fetch forecast and display

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,10 +1,14 @@
 import type { QueryClient } from "@tanstack/react-query";
-import { createRootRouteWithContext } from "@tanstack/react-router";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()({
   component: RootComponent,
 });
 
 function RootComponent() {
-  return <div>Hello! Will it rain? Don't know yet!</div>;
+  return (
+    <div className="min-h-svh bg-background text-foreground">
+      <Outlet />
+    </div>
+  );
 }
